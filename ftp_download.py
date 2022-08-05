a3a40e14c0f8d82f63633ed4b0929a8f93331363
Leaderrr <peac21@protonmail.com>
2022-08-05 07:00:10 -0700

    Adding scheduler#

--- ftp_download.py
+++ ftp_download.py
@@ -1,16 +1,40 @@
 import ftplib
+import datetime
+
+
+# function download files from specific date
+def download_date(ftp, date_entered):
+    for filename in ftp.nlst("MED_DATA_" + date_entered + "*.csv"):
+        path = r"C:\Users\Student\Documents\Temp\\" + filename
+
+        with open(path, "wb") as file:
+            ftp.retrbinary(f"RETR {filename}", file.write)
+            file.close()
+
+
+# function to download all files not previously downloaded
+def download_all(ftp, previously_downloaded, files_downloaded):
+    file = open(previously_downloaded, "w")
+    lines = file.readlines()
+
+    print(lines)
+    print(type(lines))
+
 
 # Open ftp connection
 ftp = ftplib.FTP('192.168.0.132', 'student', 'password')
 
+date_entered = input("Enter data, format YYYYMMDD (if blank, default is today). Enter \"All\" to download all files:\n")
 
-# List the files in the current directory
+files_downloaded = []
 
-date = input("Enter data, format YYYYMMDD:\n")
+if date_entered == "":
+    date_entered = datetime.date.today().strftime("%Y%m%d")
 
-for filename in ftp.nlst("MED_DATA_" + date + "*.csv"):
-    path = r"C:\Users\Student\Documents\Temp\\" + filename
+if date_entered.upper() == "ALL":
+    print("Downloading all")
+    download_all(ftp, r"C:\Users\Student\Documents\Temp\downloaded_files.txt")
+else:
+    download_date(ftp, date_entered)
 
-    with open(path, "wb") as file:
-        ftp.retrbinary(f"RETR {filename}", file.write)
-        file.close()
+# print("Files downloaded are: " + files_downloaded)
