027e9c5e0e5fb8730fe603bc7f30a7b6ee1c5855
Daniel P1 <peac21@protonmail.com>
2022-08-15 09:16:22 -0700

    Created server and completed download for date. Working on scheduler.

--- ftp_download.py
+++ ftp_download.py
@@ -5,8 +5,8 @@ import datetime
 # function download files from specific date
 def download_date(ftp, date_entered):
     for filename in ftp.nlst("MED_DATA_" + date_entered + "*.csv"):
-        path = r"C:\Users\Student\Documents\Temp\\" + filename
-
+        path = "/home/ftpclient/Documents/FTP/" + filename
+        
         with open(path, "wb") as file:
             ftp.retrbinary(f"RETR {filename}", file.write)
             file.close()
@@ -22,7 +22,9 @@ def download_all(ftp, previously_downloaded, files_downloaded):
 
 
 # Open ftp connection
-ftp = ftplib.FTP('192.168.0.132', 'student', 'password')
+print("Connecting to FTP")
+ftp = ftplib.FTP('192.168.200.128', 'ftp_user', 'password')
+print("Connected to FTP")
 
 date_entered = input("Enter data, format YYYYMMDD (if blank, default is today). Enter \"All\" to download all files:\n")
 
@@ -35,6 +37,7 @@ if date_entered.upper() == "ALL":
     print("Downloading all")
     download_all(ftp, r"C:\Users\Student\Documents\Temp\downloaded_files.txt")
 else:
+    print("Downloading for date {date_entered}")
     download_date(ftp, date_entered)
 
 # print("Files downloaded are: " + files_downloaded)
