f7b2c45abfde3870b2e759e2c1211a2170aebd6a
Daniel P1 <peac21@protonmail.com>
2022-08-15 10:22:44 -0700

    Scheduler implemented

--- ftp_download.py
+++ ftp_download.py
@@ -1,24 +1,32 @@
 import ftplib
-import datetime
+from datetime import datetime
 
+
+def download_file(ftp, filename):
+    path = "/home/ftpclient/Documents/FTP/" + filename
+    
+    with open(path, "wb") as file:
+        ftp.retrbinary(f"RETR {filename}", file.write)
+        file.close()
+    
 
 # function download files from specific date
 def download_date(ftp, date_entered):
     for filename in ftp.nlst("MED_DATA_" + date_entered + "*.csv"):
-        path = "/home/ftpclient/Documents/FTP/" + filename
-        
-        with open(path, "wb") as file:
-            ftp.retrbinary(f"RETR {filename}", file.write)
-            file.close()
+        download_file(ftp, filename)
 
 
 # function to download all files not previously downloaded
-def download_all(ftp, previously_downloaded, files_downloaded):
-    file = open(previously_downloaded, "w")
-    lines = file.readlines()
-
-    print(lines)
-    print(type(lines))
+def download_all(ftp, previously_downloaded):
+    file = open(previously_downloaded, "r+")
+    files_downloaded = file.readlines()
+    
+    for filename in ftp.nlst("MED_DATA_*.csv"):
+        if filename + "\n" not in files_downloaded:
+            print('Downloading: ' + filename + "\n")
+            download_file(ftp, filename)
+            file.write(filename)
+    file.close()
 
 
 # Open ftp connection
@@ -28,16 +36,15 @@ print("Connected to FTP")
 
 date_entered = input("Enter data, format YYYYMMDD (if blank, default is today). Enter \"All\" to download all files:\n")
 
-files_downloaded = []
-
 if date_entered == "":
-    date_entered = datetime.date.today().strftime("%Y%m%d")
+    print("Downloading files from today only.")
+    date_entered = datetime.today().strftime("%Y%m%d")
 
 if date_entered.upper() == "ALL":
     print("Downloading all")
-    download_all(ftp, r"C:\Users\Student\Documents\Temp\downloaded_files.txt")
+    download_all(ftp, "/home/ftpclient/Documents/FTP/files_downloaded.txt")
 else:
-    print("Downloading for date {date_entered}")
+    print("Downloading for date: " + datetime.strptime(date_entered, "%Y%m%d").strftime("%d/%m/%Y"))
     download_date(ftp, date_entered)
 
-# print("Files downloaded are: " + files_downloaded)
+print('All files downloaded')
