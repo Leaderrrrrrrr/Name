778a71fd377fba4402f515390f74cb04e4296609
Daniel P1 <peac21@protonmail.com>
2022-08-17 12:46:38 -0700

    Edited to use cli

--- ftp_download.py
+++ ftp_download.py
@@ -1,5 +1,6 @@
 import ftplib
 from datetime import datetime
+import sys
 
 
 #function to download file to client machine from server
@@ -48,21 +49,19 @@ print("Connecting to FTP")
 ftp = ftplib.FTP('192.168.200.128', 'ftp_user', 'password')
 print("Connected to FTP")
 
-#%% get information from user
-
-
-date_entered = input("""Enter data, format YYYYMMDD (if blank, default is today).
-Enter \"All\" to download all files:\n""")
-FILES_DOWNLOADED_LOCATION = "/home/ftpclient/Documents/FTP/files_downloaded.txt"
 
 
 #%% run download
 
-#get current date if required
-if date_entered == "":
+FILES_DOWNLOADED_LOCATION = "/home/ftpclient/Documents/FTP/files_downloaded.txt"
+
+#if no args entered, download data for today
+if len(sys.argv) == 1:
     print("Downloading files from today only.\n")
     date_entered = datetime.today().strftime("%Y%m%d")
-
+else:
+    date_entered = sys.argv[1]
+    
 if date_entered.upper() == "ALL":
     print("Downloading all\n")
     download_all(ftp, FILES_DOWNLOADED_LOCATION)
@@ -72,3 +71,7 @@ else:
     download_date(ftp, date_entered, FILES_DOWNLOADED_LOCATION)
 
 print('All files downloaded')
+
+
+
+
