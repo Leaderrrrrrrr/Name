b007bfa11315586de824fb6cd3cd6222206a017c
randname122 <110673280+randname122@users.noreply.github.com>
2022-08-19 10:11:09 +0100

    Update Download GUI.py
    Fixed bug

--- Download GUI.py
+++ Download GUI.py
@@ -10,17 +10,23 @@ entry1 = tk.Entry (root)
 canvas1.create_window(200, 140, window=entry1)
 
 def getSquareRoot ():  
-    x1 = entry1.get()
     
-    label1 = tk.Label(root, text= float(x1)**0.5)
-    canvas1.create_window(200, 230, window=label1)
+    subprocess.check_call([sys.executable, "ftp_download.py" + entry1.get()])
+    #label1 = tk.Label(root, text= float(x1)**0.5)
+    #canvas1.create_window(200, 230, window=label1)
 
 def returnToMainMenu():
     subprocess.check_call([sys.executable, "Main Menu.py"])
     
     
 button1 = tk.Button(text='Download (leave blank to get todays data', command=getSquareRoot)
-canvas1.create_window(200, 180, window=button1)
+
+button1.pack(
+     ipadx=5,
+    ipady=5,
+    expand=True
+)
+#canvas1.create_window(200, 180, window=button1)
 
 return_to_main_menu_button = tk.Button(
     root,
