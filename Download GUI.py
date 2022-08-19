e3e1fe8dbff9a9d0a322e8fd718439e4c5dd4b2a
randname122 <110673280+randname122@users.noreply.github.com>
2022-08-19 08:52:16 +0100

    Update Download GUI.py
    Added a button which allows the use to return to the main menu

--- Download GUI.py
+++ Download GUI.py
@@ -1,7 +1,8 @@
 import tkinter as tk
-
+import subprocess
+import sys
 root= tk.Tk()
-
+root.title('Download')
 canvas1 = tk.Canvas(root, width = 400, height = 300)
 canvas1.pack()
 
@@ -13,8 +14,22 @@ def getSquareRoot ():
     
     label1 = tk.Label(root, text= float(x1)**0.5)
     canvas1.create_window(200, 230, window=label1)
+
+def returnToMainMenu():
+    subprocess.check_call([sys.executable, "Main Menu.py"])
+    
     
 button1 = tk.Button(text='Download (leave blank to get todays data', command=getSquareRoot)
 canvas1.create_window(200, 180, window=button1)
 
+return_to_main_menu_button = tk.Button(
+    root,
+    text='Return to Main Menu',
+    command=lambda:[root.destroy(),returnToMainMenu()]
+)
+return_to_main_menu_button.pack(
+    ipadx=5,
+    ipady=5,
+    expand=True
+)
 root.mainloop()
