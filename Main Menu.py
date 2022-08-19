2d841293b728c2d5a4a3e8df6402e1ccb2342136
randname122 <110673280+randname122@users.noreply.github.com>
2022-08-19 08:53:30 +0100

    Update Main Menu.py
    Added code to close main menu when exit is pressed and made it so the download gui opens when the user presses the button on the main menu to open it and this also closes the main menu

--- Main Menu.py
+++ Main Menu.py
@@ -1,27 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
-
+import subprocess
+import sys
 # root window
 root = tk.Tk()
-win = tk.Tk()
+##win = tk.Tk()
 root.geometry('1920x1080')
 root.resizable(False, False)
-root.title('Button Demo')
+root.title('Main Menu')
 
 # exit button
-exit_button = ttk.Button(
-    root,
-    text='Exit',
-    command=lambda: root.quit()
-)
+def open_download():
+    subprocess.check_call([sys.executable, "Download GUI.py"])
+    
 
-exit_button.pack(
-    ipadx=5,
-    ipady=5,
-    expand=True
-)
 
-exit_button.place(x=100, y=500) 
 
 
 downloaded_files_button = ttk.Button(
@@ -34,5 +27,31 @@ downloaded_files_button.pack(
     ipady=5,
     expand=True
 )
+
+download_files_button = ttk.Button(
+    root,
+    text='Download Files',
+    command=lambda:[root.destroy(),open_download()]
+)
+download_files_button.pack(
+    ipadx=5,
+    ipady=5,
+    expand=True
+)
+
+exit_button = ttk.Button(
+    root,
+    text='Exit',
+    command=lambda:[root.destroy()]
+)
+
+exit_button.pack(
+    ipadx=5,
+    ipady=5,
+    expand=True
+)
+
+#exit_button.place(x=100, y=500) 
+
 
 root.mainloop()
