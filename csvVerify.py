f56c9f812c3f92a0e6f55668daf19b02c61d41aa
Scott Ryan <scotty7700@gmail.com>
2022-08-19 08:05:36 +0100

    csvVerify date check in file name and record checking and formatting implemented

--- csvVerify.py
+++ csvVerify.py
@@ -3,13 +3,15 @@ import os
 import logging
 import pandas as pd
 
+CHECKED_PATH = "./checked"
+
 if len(sys.argv) < 2: # The file takes at least one file as an argument
     logging.error(" Must have at least one argument")
-    logging.log("Exiting...")
+    logging.log(0, "Exiting...")
     exit()
 
 for file in sys.argv[1:]: # For each file given as an argument
-    logging.log(f"Validating file %s" % file)
+    logging.log(0, f"Validating file %s" % file)
 
     filename = file.split("/")[-1]
 
@@ -17,11 +19,11 @@ for file in sys.argv[1:]: # For each file given as an argument
     try:
         data = pd.read_csv(file)
         logging.log(0, "file read")
-        logging.log("removing temp file")   # !WITH THE TEMP FILE REMOVED, CONTINUE WILL DELETE THE FILE
+        logging.log(0, "removing temp file")   # !WITH THE TEMP FILE REMOVED, CONTINUE WILL DELETE THE FILE
         os.remove(file)
     except:
         logging.error("File not readable")
-        logging.log("Deleting file...")
+        logging.log(0, "Deleting file...")
         os.remove(file)
         continue
 
@@ -29,7 +31,13 @@ for file in sys.argv[1:]: # For each file given as an argument
     if filename[:9] != "MED_DATA_" or filename[23:] != ".csv":
         logging.error("Invalid file name")
         continue
-    #if filename[:9] !=                 TODO: check date of file
+    if int(filename[9:13]) > 3000 or int(filename[9:13]) < 1970 or int(filename[13:15]) > 12 or int(filename[13:15]) < 1 or int(filename[15:17]) > 31 or int(filename[15:17]) < 1:
+        logging.error("Invalid date in file name")
+        continue
+
+    if int(filename[17:19]) > 24 or int(filename[17:19]) < 0 or int(filename[19:21]) > 59 or int(filename[19:21]) < 0 or int(filename[21:23]) > 59 or int(filename[21:23]) < 0:
+        logging.error("Invalid time in file name")
+        continue
 
     # Check number of columns
     if len(data.columns) == 12:
@@ -48,7 +56,7 @@ for file in sys.argv[1:]: # For each file given as an argument
             continue
 
     # timestamp checking
-    for timestamp in range(1, len(data["timestamp"])):
+    for timestamp in data["timestamp"]:
         parts = timestamp.split(":")
         if int(parts[0]) > 24 or int(parts[0]) < 0 or len(parts[0]) != 2:
             logging.error("Invalid hour")
@@ -61,14 +69,14 @@ for file in sys.argv[1:]: # For each file given as an argument
             continue
 
     # Reading checking
-    #for cols in data.columns[2:]:
-    #    for i in data[cols]:
-
-    # n = float("{0:.3f}".format(n))
+    for cols in data.columns[2:]:
+        for i in range(data[cols]):
+            if data[cols][i] < 0 or data[cols][i] > 9.9:
+                logging.error("value out of bounds")
+                continue
+            data[cols][i] = float("{0:.3f}".format(data[cols][i]))
 
 
     # Save file to checked folder
-    logging.log("File Approved: sending to checked folder")
-    pd.to_csv("./checked/" + filename)      # If the file already exists, it will be overwritten
-
-    logging.log("removing temp file")
+    logging.log(0, "File Approved: sending to checked folder")
+    pd.to_csv(CHECKED_PATH + filename)      # If the file already exists, it will be overwritten
